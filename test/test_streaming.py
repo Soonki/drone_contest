f0e986a98691bdf66688d7ff74f4203c5a9df66a
sakashita <sakashita.t@gmail.com>
2019-07-11 21:31:57 +0900

    raspberrypi streaming用test

--- test/test_streaming.py
+++ test/test_streaming.py
@@ -1,12 +1,42 @@
-import subprocess
-import shlex
-
-#2019/07/10作成．カメラをブラウザでストリーミングできるようにするテスト
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import subprocess
+import commands
+import shlex
+import time
+import threading
+#2019/07/10作成．カメラをブラウザでストリーミングできるようにするテスト
 
-if __name__ == '__main__':
-    cmd = "python test_streaming2.py"
+def stream():
+    cmd = "sh start_server.sh" 
+    #cmd = "usr/local/bin/mjpg_streamer -i \"input_raspicam.so -x 640 -y 480 -fps 15 -q 80\" -o \"output_http.so -p 8080 -w /usr/local/share/mjpg-streamer/www\" -b"
+    #-bでバックグラウンドで実行
+    #https://qiita.com/ego/items/3d23cda713f29f0dd141
     cmd = shlex.split(cmd)
     ret = subprocess.check_output(cmd)
     print(ret)
+    time.sleep(3)
+    print("OK")
+    #cmd="sh end_server.sh"
+    cmd2="pidof mjpg_streamer"
+    cmd2 = shlex.split(cmd2)
+    ret2 = subprocess.check_output(cmd2)
+    print(ret2)
+    cmd="kill -9 "+ret2
+    cmd = shlex.split(cmd)
+    ret = subprocess.check_output(cmd)
+    print(ret)
+    print("END")
+if __name__ == '__main__':
+    cmd = ['python', 'test_streaming2.py']
+    #cmd = shlex.split(cmd)
+    ret = subprocess.check_output(cmd)
+    print(ret)
+   # t=threading.Thread(target=stream)
+   # t.start()   
+
+    print(ret)
+    print("サーバー立てても動きます")
+    time.sleep(1)
+    print("待って！")
+    stream()
