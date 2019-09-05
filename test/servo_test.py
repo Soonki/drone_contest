6b0fca9806ae9ead397cf423689ac11a29e1069f
sakashita <sakashita.t@gmail.com>
2019-09-05 13:00:29 +0900

    raspberrypi サーボ調整　中間値設定

--- test/servo_test.py
+++ test/servo_test.py
@@ -9,11 +9,24 @@ gpio_pin0 = 18
 pi = pigpio.pi()
 pi.set_mode(gpio_pin0, pigpio.OUTPUT)
 
+pwm1=113600
+pwm2= 70000
+pwm3= 26100
+#サーボPWM幅
+#上限：110000強
+#下限： 27000弱
+
 # GPIO18: 2Hz、duty比0.5
 #pi.hardware_PWM(gpio_pin0, 50, 100000)
-pi.hardware_PWM(gpio_pin0, 50,27000)
-print("27000")
-time.sleep(3)
-pi.hardware_PWM(gpio_pin0, 50, 78000)
-print("78000")
+pi.hardware_PWM(gpio_pin0, 50,pwm1)
+print(pwm1)
+time.sleep(2)
+pi.hardware_PWM(gpio_pin0, 50,pwm2)
+print(pwm2)
+time.sleep(2)
+pi.hardware_PWM(gpio_pin0, 50, pwm3)
+print(pwm3)
+time.sleep(2)
+pi.hardware_PWM(gpio_pin0, 50, pwm1)
+print(pwm1)
 pi.stop()
